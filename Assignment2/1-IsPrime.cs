8ddcc6a56af1eca010fd331c3719ad92e2cbe47a
Dongrong Su <116546724+Norqing@users.noreply.github.com>
2024-03-19 21:44:39 +0800

    Add files via upload
    编写程序输出用户指定数据的所有素数因子。

---
+++ Assignment2/1-IsPrime.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FindPrimeNumApplication
+{
+    class FindPrimeNum
+    {
+        int input;
+
+        public FindPrimeNum(int getin)
+        {
+            input = getin;
+        }
+
+        public bool IsPrime(int num)
+        {
+            if (num <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void FindPrimeFactors()
+        {
+            Console.Write($"'{input}'的所有素数因子为：");
+
+            for (int i = 2; i <= input; i++)
+            {
+                if (input % i == 0 && IsPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("请输入一个整数：");
+            string getin = Console.ReadLine();
+
+            if (int.TryParse(getin, out int number))
+            {
+                FindPrimeNum primeFinder = new FindPrimeNum(number);
+                primeFinder.FindPrimeFactors();
+            }
+            else
+            {
+                Console.WriteLine("输入无效，请输入一个整数。");
+            }
+        }
+    }
+}
